4bb47cdac18c4afb896d77558a4aee8ee8522321
Ryan Helsing <ryanhelsing@gmail.com>
2017-03-15 13:48:52 -0400

    user map

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -3,8 +3,17 @@ class Feedback < ApplicationRecord
 
   def self.users
     {
-      "h828hd2" => "Ryan H",
-      "dg828gd" => "Steve H",
+      "9569d8a4" => "Ryan H",
+      "bd14fa3f" => "Steve H",
+      "5acc394b" => "Blair R",
+      "fdfefe1f" => "Mariah H",
+      "8ca68037" => "Daniel B",
+      "422d906e" => "Rickard S",
+      "1fc3f873" => "Joe W",
+      "51f57643" => "David E",
+      "680a3d39" => "Chartana V",
+      "8efcee63" => "Matt O",
+      "55140da4" => "Will J"
     }
   end
 
