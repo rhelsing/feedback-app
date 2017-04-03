44c24a16686abd1c3543d1cfc5df214e73cd067d
Rickard Sunden <rickard.sunden@outlook.com>
2017-04-03 15:12:51 -0400

    make it so token is saved through out after you type it

--- app/assets/javascripts/my_feedbacks.js
+++ app/assets/javascripts/my_feedbacks.js
@@ -1,11 +1,24 @@
 var MyFeedbacks = (function(){
   $(document).on('click', '#my-feedbacks', function(){
-    var val = $(this).closest('#search').find('input').val());
-    location.replace('/my_feedbacks/' + val;
+    var val = $(this).closest('#nav').find('input').val();
+    location.replace('/my_feedbacks/' + val);
   });
 
   $(document).on('click', '#new-feedback', function(){
-    var val = $(this).closest('#search').find('input').val());
-    location.replace('/feedbacks/new?token=' + val;
+    var val = $(this).closest('#nav').find('input').val();
+    location.replace('/feedbacks/new?token=' + val);
   });
+
+  $(document).on('keyup', '#nav input', function(){
+    var token = $(this).val();
+    $("#feedback_token").val(token);
+  });
+
+  $(document).on('turbolinks:load', function(){
+    var token = $("#nav input").val();
+    console.log(token);
+    $("#feedback_token").val(token);
+    console.log($("#feedback_token").val());
+  });
+
 })();
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "feedbacks#new"
-  resources :feedbacks
-  get '/my_feedbacks/:my_token' => "feedbacks#show_by_token", as: "feedbacks_with_token"
+  resources :feedbacks, except: [:edit, :update]
+  get '/my_feedbacks/:token' => "feedbacks#show_by_token", as: "feedbacks_with_token"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
